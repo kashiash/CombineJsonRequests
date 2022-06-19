d0283b186acc08ead4bf2a462b6799f1700e5b7f
kashiash <kashiash@gmail.com>
2022-06-19 19:39:55 +0200

    Initial Commit

---
+++ CombineJsonRequests/CombineJsonRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineJsonRequestsApp.swift
+//  CombineJsonRequests
+//
+//  Created by Jacek Placek on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineJsonRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
